209ffa7aacc98bf7218a686a16552dff2271e472
maxi <Bairesmoving@gmail.com>
2023-08-27 16:49:59 -0300

    tarea

---
+++ js/tarea.js
@@ -0,0 +1,23 @@
+const articulos = [
+    { id:1,
+    nombre: "mouse", 
+    origen: "China" },
+    {id:2,
+      nombre: "teclado", 
+    origen: "EE. UU." },
+    {id:3, 
+      nombre: "monitor", 
+    origen: "Corea del Sur" }
+  ];
+
+  console.log(articulos[2].origen);
+
+const pacientes= {
+  nombre:"Mariano",
+  edad: "30",
+  consultas:["Dolor de cabeza", "mareos", "dolor de cabeza"]
+}
+console.log (pacientes.consultas[2])
+
+
+
